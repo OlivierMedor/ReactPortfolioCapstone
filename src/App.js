a53a91b4bcdd19ce8dfce18431ce023f989322aa
Olivier Medor <oliviermedor@yahoo.com>
2023-12-04 10:23:20 -0500

    added header and meta tags

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
-
+import Header from './Header';
+import Footer from './Footer';
+import Main from './Main';
+import Nav
+ from './Nav';
 function App() {
   return (
-    <div className="App">Homepage</div>
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <Main></Main>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import './Header.css'
+function Header() {
+  return (
+    <>
+      <header><img className="logo" src={require('./assets/Asset 14@4x.png')} alt="logo" /></header>
+    </>
+  );
+}
+
+export default Header;
